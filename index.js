dbf5ad44eb00ec70aa3f9e5d985b1fe23592f085
Jonny Kalambay <jonnyk20@gmail.com>
2018-09-06 20:49:50 -0700

    creating seperate file

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
 const express = require('express');
 const path = require('path');
 const R = require('ramda');
+const { google } = require('googleapis');
 
 // Utilities
 const util = require('util');
 const fs = require('fs');
 
+const writeFile = util.promisify(fs.writeFile);
+const readFile = util.promisify(fs.readFile);
+
 const app = express();
-// Google Stuff
-var { google } = require('googleapis');
 
-fs.writeFile('./.env', 'Hey there!', err => {
-  if (err) {
-    return console.log(err);
+const save = async (path, str) => {
+  try {
+    await writeFile(path, str);
+    console.log('Saved!');
+  } catch (error) {
+    console.log('Error Saving:', error);
   }
+};
 
-  console.log('The file was saved!');
-});
+const read = async path => {
+  try {
+    const content = await readFile(path);
+    const object = JSON.parse(content);
+    return object;
+    console.log('Object:', object);
+  } catch (error) {
+    console.log('Error Reading:', error);
+    return null;
+  }
+};
 
 const OAuth2 = google.auth.OAuth2;
 
@@ -30,6 +45,7 @@ const clientId =
 const clientSecret = '1zMRJ1cgLXJUx4JtT1PCAArZ';
 const redirectUrl = 'http://localhost:3000/callback';
 const oauth2Client = new OAuth2(clientId, clientSecret, redirectUrl);
+const service = google.youtube('v3');
 oauth2Client.on('tokens', tokens => {
   if (tokens.refresh_token) {
     // store the refresh_token in my database!
@@ -42,56 +58,59 @@ let chatId;
 
 const port = 3000;
 
-const getChatMessages = liveChatId => {
-  const service = google.youtube('v3');
-  service.liveChatMessages
-    .list({
+const getChatMessages = async liveChatId => {
+  try {
+    const response = await service.liveChatMessages.list({
       auth: oauth2Client,
       part: 'snippet',
       liveChatId
-    })
-    .then(response => {
-      const comments = response.data.items;
-      console.log(comments[0]);
-      console.log('comments ids', R.pluck('id', comments));
-    })
-    .catch(err => {
-      console.log('The API returned an error: ' + err);
-      console.log(util.inspect(err, false, null));
-      return;
     });
+    const comments = response.data.items;
+    console.log('first comment', comments[0]);
+  } catch (error) {
+    console.log('The API returned an error: ' + error);
+  }
 };
 
-const listLiveBroadCasts = auth => {
-  const service = google.youtube('v3');
-  service.liveBroadcasts
-    .list({
+const getLatestChatId = async auth => {
+  try {
+    const response = await service.liveBroadcasts.list({
       auth,
       part: 'snippet',
       mine: true
-    })
-    .then(response => {
-      chatId = R.path(['data', 'items', 0, 'snippet', 'liveChatId'], response);
-      console.log('chatId', chatId);
-      getChatMessages(chatId);
-    })
-    .catch(err => console.log('err', err));
+    });
+    const latestChat = response.data.items[0];
+    const chatId = latestChat.snippet.liveChatId;
+    return chatId;
+  } catch (error) {
+    console.log('Error ferching broadcasts', error);
+  }
 };
 
-const authorize = auth => {
-  oauth2Client.setCredentials(auth.tokens);
-
+const authorize = ({ tokens }) => {
+  save('./tokens.json', JSON.stringify(tokens));
+  oauth2Client.setCredentials(tokens);
   console.log('Successfully authed');
-  listLiveBroadCasts(oauth2Client);
 };
 
 const getNewToken = res => {
   const authUrl = oauth2Client.generateAuthUrl({
-    access_type: 'online',
+    access_type: 'offline',
     scope: SCOPES
   });
   res.redirect(authUrl);
 };
+
+const checkAuth = async () => {
+  const tokens = await read('./tokens.json');
+  if (tokens) {
+    oauth2Client.setCredentials(tokens);
+  } else {
+    console.log('no tokens set');
+  }
+};
+
+checkAuth();
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
@@ -100,15 +119,22 @@ app.get('/auth', (req, res) => {
   getNewToken(res);
 });
 
-app.get('/callback', (req, res) => {
+app.get('/callback', async (req, res) => {
   console.log('/callback');
   const code = req.query.code;
   console.log('code', code);
-  oauth2Client
-    .getToken(code)
-    .then(authorize)
-    .catch(err => console.log('err', err));
+  try {
+    const auth = await oauth2Client.getToken(code);
+    authorize(auth);
+  } catch (error) {
+    console.log('Error in Callback', err);
+  }
   res.sendFile(path.join(__dirname + '/public/callback.html'));
+});
+
+app.get('/test', (req, res) => {
+  getLatestChatId(oauth2Client);
+  res.end('testing');
 });
 
 app.listen(port);
