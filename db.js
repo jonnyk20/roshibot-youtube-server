5ee618e1dcd82f84a1e4dd5cc2c485863b98cb7f
Jonny Kalambay <jonnyk20@gmail.com>
2018-10-16 14:32:32 +0800

    Changing db name

--- db.js
+++ db.js
@@ -9,7 +9,7 @@ const connect = app => {
       if (err) throw err;
       console.log('Database connected!');
       app.emit('ready');
-      var dbo = db.db('roshibot');
+      var dbo = db.db('heroku_t226sql1');
       dbo.collection('tokens').findOne({}, function(err, result) {
         if (err) throw err;
         console.log('token found', result);
